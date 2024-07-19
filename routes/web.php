8d835206f6894bec89b930106d34213b48decc61
chouming <bangcac24@gmail.com>
2024-07-19 14:25:05 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Adminpanel;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [Adminpanel::class, 'index']);
+Route::resource('posts', Adminpanel::class);
 
 Route::get('/', function () {
     return view('welcome');
